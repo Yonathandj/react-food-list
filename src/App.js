9613f770872f5869d4b1386689984d2f826f05b8
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2022-11-01 10:19:26 +0700

    Add element and styling Header Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Header from "./components/Header"
+import Content from "./components/Content"
+import Footer from "./components/Footer"
+
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      {/* <Content/>
+      <Footer/> */}
     </div>
   );
 }
